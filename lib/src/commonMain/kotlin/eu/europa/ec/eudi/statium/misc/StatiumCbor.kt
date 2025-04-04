38cd109e9690b818ea65d600ad6a632586766a27
babisRoutis <haralampos.routis@netcompany.com>
2025-04-04 16:09:05 +0300

    Added StatiumCbor

---
+++ lib/src/commonMain/kotlin/eu/europa/ec/eudi/statium/misc/StatiumCbor.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.statium.misc
+
+import eu.europa.ec.eudi.statium.CompressedByteArray
+import eu.europa.ec.eudi.statium.DurationAsSeconds
+import eu.europa.ec.eudi.statium.InstantAsEpocSeconds
+import kotlinx.serialization.cbor.Cbor
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.serializer
+
+public val StatiumCborSerializersModule: SerializersModule =
+    SerializersModule {
+        contextual(CompressedByteArray::class, serializer<ByteArray>())
+        contextual(InstantAsEpocSeconds::class, EpocSecondsCborSerializer)
+        contextual(DurationAsSeconds::class, DurationAsSecondsSerializer)
+    }
+
+public val StatiumCbor: Cbor =
+    Cbor(from = Cbor.CoseCompliant) {
+        ignoreUnknownKeys = true
+        alwaysUseByteString = true
+        serializersModule = StatiumCborSerializersModule
+    }
