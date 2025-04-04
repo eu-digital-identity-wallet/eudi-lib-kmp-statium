11ab7fe71456bd5302cdb14a99b2c1cf8d71c177
babisRoutis <haralampos.routis@netcompany.com>
2025-04-04 13:58:27 +0300

    Added dependency to kotlinx.serialization cbor

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -80,6 +80,7 @@ kotlin {
                 // Common dependencies
                 api(libs.kotlinx.serialization.core)
                 api(libs.kotlinx.serialization.json)
+                api(libs.kotlinx.serialization.cbor)
                 api(libs.kotlinx.coroutines.core)
                 api(libs.ktor.client.core)
                 implementation(libs.ktor.client.serialization)
