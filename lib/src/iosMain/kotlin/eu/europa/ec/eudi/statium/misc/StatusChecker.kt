44abea6211cde795c2765cb5e02bbf8df1dc5392
dtsiflit <dimitri.tsiflitzis@netcompany.com>
2025-06-17 13:37:07 +0300

    spotless fixes

--- lib/src/iosMain/kotlin/eu/europa/ec/eudi/statium/misc/StatusChecker.kt
+++ lib/src/iosMain/kotlin/eu/europa/ec/eudi/statium/misc/StatusChecker.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.europa.ec.eudi.statium.misc
 
 import eu.europa.ec.eudi.statium.GetStatus
@@ -24,7 +39,7 @@ public class StatusChecker {
     public fun checkStatus(
         statusReference: StatusReference,
         atTime: String,
-        callback: (StatusCheckResult) -> Unit
+        callback: (StatusCheckResult) -> Unit,
     ) {
         scope.launch {
             try {
@@ -36,7 +51,6 @@ public class StatusChecker {
                 }
                 val matches = actualStatus == Status.Valid
                 callback(StatusCheckResult.Success(matches))
-
             } catch (e: Exception) {
                 callback(StatusCheckResult.Failure(e.message ?: "Unknown error"))
             }
@@ -50,7 +64,7 @@ public class StatusChecker {
             clock,
             httpClientFactory,
             verifySignature,
-            Duration.ZERO
+            Duration.ZERO,
         )
         val decompress = platformDecompress()
         return GetStatus(getStatusListToken, decompress)
