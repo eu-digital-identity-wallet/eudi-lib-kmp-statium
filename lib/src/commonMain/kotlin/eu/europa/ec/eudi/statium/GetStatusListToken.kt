050f7d1136db39be15439bcbf790567ac8d43425
babisRoutis <haralampos.routis@netcompany.com>
2025-08-29 09:56:26 +0300

    Added support for parsing and validating CWT-encoded status list tokens

--- lib/src/commonMain/kotlin/eu/europa/ec/eudi/statium/GetStatusListToken.kt
+++ lib/src/commonMain/kotlin/eu/europa/ec/eudi/statium/GetStatusListToken.kt
@@ -15,6 +15,8 @@
  */
 package eu.europa.ec.eudi.statium
 
+import eu.europa.ec.eudi.statium.cose.ParseCwt
+import eu.europa.ec.eudi.statium.cose.ParseCwtUsingKotlinx
 import eu.europa.ec.eudi.statium.http.GetStatusListTokenKtorOps
 import eu.europa.ec.eudi.statium.jose.jwtHeaderAndPayload
 import eu.europa.ec.eudi.statium.misc.runCatchingCancellable
@@ -22,6 +24,7 @@ import io.ktor.client.HttpClient
 import kotlinx.serialization.Required
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.cbor.CborLabel
 import kotlin.time.Clock
 import kotlin.time.Duration
 import kotlin.time.Instant
@@ -33,13 +36,39 @@ public fun interface GetStatusListToken {
     public companion object {
         /**
          * Factory method that creates an instance of [GetStatusListToken]
+         * that works with JWT encoded status list tokens
          */
         public fun usingJwt(
             clock: Clock,
             httpClient: HttpClient,
             verifyStatusListTokenSignature: VerifyStatusListTokenSignature,
             allowedClockSkew: Duration = Duration.ZERO,
-        ): GetStatusListToken = GetStatusListTokenUsingJwt(clock, httpClient, verifyStatusListTokenSignature, allowedClockSkew)
+        ): GetStatusListToken =
+            GetStatusListTokenUsingJwt(
+                clock,
+                httpClient,
+                verifyStatusListTokenSignature,
+                allowedClockSkew,
+            )
+
+        /**
+         * Factory method that creates an instance of [GetStatusListToken]
+         * that works with CWT encoded status list tokens
+         */
+        public fun usingCwt(
+            clock: Clock,
+            httpClient: HttpClient,
+            verifyStatusListTokenSignature: VerifyStatusListTokenSignature,
+            parseCwt: ParseCwt<ByteArray, ByteArray> = ParseCwtUsingKotlinx,
+            allowedClockSkew: Duration = Duration.ZERO,
+        ): GetStatusListToken =
+            GetStatusListTokenUsingCwt(
+                clock,
+                httpClient,
+                verifyStatusListTokenSignature,
+                parseCwt,
+                allowedClockSkew,
+            )
     }
 }
 
@@ -93,4 +122,52 @@ internal class GetStatusListTokenUsingJwt(
             require(algorithm.isNotBlank()) { "alg can't be blank" }
         }
     }
+}
+
+internal class GetStatusListTokenUsingCwt(
+    private val clock: Clock,
+    private val httpClient: HttpClient,
+    private val verifySignature: VerifyStatusListTokenSignature,
+    parseCwt: ParseCwt<ByteArray, ByteArray>,
+    private val allowedClockSkew: Duration,
+) : GetStatusListToken, GetStatusListTokenKtorOps, StatusListTokenValidations {
+
+    init {
+        require(allowedClockSkew >= Duration.ZERO) { "allowedClockSkew must be >= 0" }
+    }
+
+    private val parseCwt: ParseCwt<ProtectedHeader, StatusListTokenClaims> = ParseCwt.map(parseCwt)
+
+    override suspend fun invoke(uri: String, at: Instant?): Result<StatusListTokenClaims> =
+        runCatchingCancellable {
+            val unverifiedCwt = fetchToken(uri, at)
+            val validationTime = at ?: clock.now()
+            verifySignature(unverifiedCwt, validationTime)
+            val (header, claims) = parseCwt(unverifiedCwt.encodeToByteArray())
+            checkNotNull(claims) { "Missing claims in CWT" }
+            header.ensureTypeIsStatusListCwt()
+            claims.ensureValid(expectedSubject = uri, validationTime, allowedClockSkew = allowedClockSkew)
+        }
+
+    private suspend fun fetchToken(uri: String, at: Instant?): String =
+        httpClient.getStatusListToken(uri, StatusListTokenFormat.CWT, at).getOrThrow()
+
+    private suspend fun verifySignature(unverifiedCwt: String, verificationTime: Instant) {
+        verifySignature(unverifiedCwt, StatusListTokenFormat.CWT, verificationTime)
+            .getOrElse { error -> throw IllegalStateException("Invalid CWT signature", error) }
+    }
+
+    private fun ProtectedHeader.ensureTypeIsStatusListCwt() {
+        checkNotNull(type) {
+            "Missing `type` from COSE header"
+        }
+        check(type == TokenStatusListSpec.MEDIA_SUBTYPE_STATUS_LIST_CWT) {
+            "Wrong `type (16)` expecting ${TokenStatusListSpec.MEDIA_SUBTYPE_STATUS_LIST_CWT} found $type"
+        }
+    }
+
+    @Serializable
+    internal data class ProtectedHeader(
+        @CborLabel(16) val type: String? = null,
+    )
 }
---
+++ lib/src/commonTest/kotlin/eu/europa/ec/eudi/statium/cose/ParseCwtHeaderAndPayloadTest.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.statium.cose
+
+import eu.europa.ec.eudi.statium.GetStatusListTokenUsingCwt
+import eu.europa.ec.eudi.statium.StatusList
+import eu.europa.ec.eudi.statium.StatusListTokenClaims
+import eu.europa.ec.eudi.statium.TokenStatusListSpec
+import eu.europa.ec.eudi.statium.misc.StatiumCbor
+import kotlinx.coroutines.test.runTest
+import kotlinx.serialization.decodeFromByteArray
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ParseCwtHeaderAndPayloadTest {
+    @Test
+    fun test() {
+        val hex = "a2646269747301636c73744a78dadbb918000217015d"
+        val statusList = StatiumCbor.decodeFromByteArray<StatusList>(hex.hexToByteArray())
+        println(statusList)
+        println(statusList.compressedList.byteArrayToUnicode())
+    }
+
+    @Test
+    fun testCwtUsingCoseSign1() = runTest {
+        val input = """
+            d2845820a2012610781a6170706c69636174696f6e2f7374617475736c6973742b63
+            7774a1044231325850a502782168747470733a2f2f6578616d706c652e636f6d2f73
+            74617475736c697374732f31061a648c5bea041a8898dfea19fffe19a8c019fffda2
+            646269747301636c73744a78dadbb918000217015d584027d5535dfe0a33291cc9bf
+            b41053ad2493c49d1ee4635e12548a79bac92916845fee76799c42762f928441c5c3
+            44e3612381e0cf88f2f160b3e1f97728ec8403
+        """.trimIndent().replace("\n", "").hexToByteArray()
+
+        val (protectedHeader, statusListTokenClaims) =
+            cwtHeaderAndPayload<GetStatusListTokenUsingCwt.ProtectedHeader, StatusListTokenClaims>(input).getOrThrow()
+
+        assertEquals(
+            GetStatusListTokenUsingCwt.ProtectedHeader(TokenStatusListSpec.MEDIA_TYPE_APPLICATION_STATUS_LIST_CWT),
+            protectedHeader,
+        )
+
+        println(protectedHeader)
+        println(statusListTokenClaims)
+    }
+
+    private fun ByteArray.byteArrayToUnicode(): String {
+        return joinToString("") { byte ->
+            if (byte in 32..126) { // Printable ASCII range
+                byte.toInt().toChar().toString()
+            } else {
+                "\\u${byte.toUByte().toString(16).padStart(4, '0')}"
+            }
+        }
+    }
+}
